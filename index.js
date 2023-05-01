7ef11926669b00a03607ec7e4653e0cf59b0d08a
ohtero <91533120+Tuhto@users.noreply.github.com>
2023-05-01 23:18:38 +0300

    bug fixing

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 mongoose.connect('mongodb+srv://tester:testerpass1@cluster0.bc5yosh.mongodb.net/music')
   .then(() => {
     app.listen (PORT, () => {
-      console.log(`Connected to db and  istening on port ${PORT}`)
+      console.log(`Connected to db`);
     })
   })
   .catch(e => console.error(e));
@@ -31,3 +31,7 @@ app.post('/api/add', createEntry);
 app.patch('/api/update/:id', updateEntry);
 
 app.delete('/api/delete/:id', deleteEntry);
+
+app.listen(PORT, () => {
+  console.log(`Listening to port ${PORT}`)
+});
