3d4bff4bdced1b07298842701ec13a684368a998
ohtero <91533120+Tuhto@users.noreply.github.com>
2023-05-01 23:15:08 +0300

    fixing ports

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const {
   updateEntry,
   deleteEntry } = require('./controllers/albumController')
 
-const PORT = process.env.port || 3000
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
